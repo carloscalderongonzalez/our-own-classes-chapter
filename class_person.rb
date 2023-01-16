92d5aac5d1f6dce210e149fd857873bc8ec4755e
ccalderong94 <carloscalderon@chicagobooth.edu>
2023-01-16 15:42:12 +0000

    1.0

--- class_person.rb
+++ class_person.rb
@@ -24,3 +24,30 @@ end
 #  other_person.age
 # Output:
 #=>  45
+require "date"
+Date.new
+class Person
+  attr_accessor :first_name
+  attr_accessor :last_name
+  attr_accessor :birthdate
+
+  def full_name
+    return self.first_name + " " + self.last_name
+  end
+
+  def age
+    today = Date.today
+    birth = Date.parse(birthdate)
+    diff = today - birth
+    years = diff/365
+    years = years.to_i
+    return years
+  end
+end
+
+dev = Person.new
+dev.first_name = "James"
+dev.last_name = "Baldwin"
+dev.birthdate = "November 16, 1953"
+p dev.full_name
+p dev.age
